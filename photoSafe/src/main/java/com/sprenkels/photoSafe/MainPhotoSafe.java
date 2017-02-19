bfa6af0e3fce7dedf1a5357ed4fb7d3011a5e1a3
Ron Sprenkels <ron.sprenkels@gmail.com>
2017-02-19 23:42:15 +0100

    zoekmethode moet sneller en meer exact.

--- photoSafe/src/main/java/com/sprenkels/photoSafe/MainPhotoSafe.java
+++ photoSafe/src/main/java/com/sprenkels/photoSafe/MainPhotoSafe.java
@@ -3,6 +3,7 @@ package com.sprenkels.photoSafe;
 public class MainPhotoSafe {
 
 	public static void main(String[] args) {
-		PhotoSafe ps = new PhotoSafe();
+		PhotoSafe ps = new PhotoSafe("I:/FotoVideoMASTER");
+		ps.checkDiff("../additions");
 	}
 }
--- photoSafe/src/main/java/com/sprenkels/photoSafe/PhotoSafe.java
+++ photoSafe/src/main/java/com/sprenkels/photoSafe/PhotoSafe.java
@@ -8,29 +8,56 @@ import org.apache.log4j.Logger;
 public class PhotoSafe {
 	String baseDir;
 	final static Logger log = Logger.getLogger(PhotoSafe.class);
-	HashSet files; 
+	HashSet<File> files; 
 	
 	PhotoSafe() {
-		baseDir =  "../fotos";
-		files = new HashSet();
-		init(baseDir);
+		this("../fotos");
 	}
 	
+	PhotoSafe(String baseDir) {
+		this.baseDir =  baseDir;		
+		files = new HashSet<File>();
+		init();
+	}
+	
+	public void
+	checkDiff(String diffDir) {
+		File[] faFiles = new File(diffDir).listFiles();
+		for(File file: faFiles) {
+			if(file.isDirectory()){
+				checkDiff(file.getPath());
+			} else {
+				if(file.getName().matches("^(.*?)")){
+					File foundPsFile = null;
+					for (File psFile : files) {
+						if(file.length() == psFile.length()) {
+							foundPsFile = psFile;
+							break;
+						}
+					}
+					log.debug("Diffing file " + (foundPsFile == null ? "not found" : foundPsFile.getPath()) + " " + file.length() + " " + file.getPath());
+				}				
+			}
+		}
+	}
+
 	void
 	init(String baseDir) {
 		File[] faFiles = new File(baseDir).listFiles();
 		for(File file: faFiles) {
 			if(file.isDirectory()){
-				init(file.getAbsolutePath());
+				init(file.getPath());
 			} else {
 				if(file.getName().matches("^(.*?)")){
-					System.out.println(file.getAbsolutePath());
+					log.debug("found file " + file.length() + " " + file.getPath());
+					files.add(file);
 				}				
 			}
 		}
 	}
 	
 	private void init() {
-		log.debug("init " + baseDir);
+		log.debug("initializing from baseDir " + baseDir);
+		init(baseDir);
 	}
 }
